16390348d117bc7a53cb18918aa1e4503d8b2250
zhoucqs <1544343319@qq.com>
2022-02-25 20:14:28 +0800

    添加项目文件。

---
+++ W1HW2App/dayprinter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW2App
+{
+    internal class dayprinter
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday", "sunday" };
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+        }
+    }
+}
---
+++ W1HW3App/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要判断的年份");
+            int year = Convert.ToInt32(Console.ReadLine());
+            //年份能够被400整除。（2000）
+            //年份能被4整除，但是不能被100整除。（2008）
+
+            //逻辑与的优先级高于逻辑或,最好关系运算式加括号便于阅读
+
+            bool result = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
